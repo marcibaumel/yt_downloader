ef1a20302d69cd660b6601d82636c1c522b4d4f3
Marci <marcibaumel@gmail.com>
2024-05-20 11:05:56 +0200

    init

---
+++ backend/Controllers/VideoController.cs
@@ -0,0 +1,17 @@
+﻿// Ignore Spelling: backend
+
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace backend.Controllers
+{
+    public class VideoController : Controller
+    {
+        [HttpGet("DownloadVideo/{link}")]
+        public ActionResult<String> DownloadVideo(string link)
+        {
+            Log.Information("Given link "+ link);
+            return Ok(link);
+        }
+    }
+}
---
+++ backend/Program.cs
@@ -0,0 +1,40 @@
+
+using Serilog;
+using Serilog.Formatting.Json;
+
+namespace backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.Console().WriteTo.File("logs/myLog-.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
